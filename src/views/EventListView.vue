37e3100f936dc2fb34f04f483f334eb136266994
Yanawarut Kanwana <yanawarood.yk@gmail.com>
2022-07-30 16:34:09 +0700

    around 2.7-2.8

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -6,6 +6,16 @@
       :key="event.id"
       :event="event"
     ></EventCard>
+    <router-link
+      :to="{ name: 'EventList', query: { page: page - 1 } }"
+      rel="prev"
+      v-if="page != 1"
+    ></router-link>
+    <router-link
+      :to="{ name: 'EventList', query: { page: page + 1 } }"
+      rel="next"
+      v-if="hasNextPage"
+    ></router-link>
   </div>
 </template>
 
@@ -13,6 +23,8 @@
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
+import { watchEffect } from '@vue/runtime-core'
+
 export default {
   name: 'EventListView',
   props: {
@@ -26,17 +38,30 @@ export default {
   },
   data() {
     return {
-      events: null
+      events: null,
+      totalEvents: 0 // add thiis for store totalevent
     }
   },
   created() {
-    EventService.getEvents(2, this.page)
-      .then((response) => {
-        this.events = response.data
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+    watchEffect(() => {
+      EventService.getEvents(2, this.page)
+        .then((response) => {
+          this.events = response.data
+          this.totalEvents = response.headers['x-total-count'] // <-- store func
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    })
+  },
+  computed: {
+    hasNextPage() {
+      //first calculate total pages
+      let totalPages = Math.ceil(this.totalEvents / 2) // 2 is events per pages
+
+      //then check to see if the current page is less than a total pages
+      return this.page < totalPages
+    }
   }
 }
 </script>
